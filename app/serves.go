7507f0c7646cb4bd79bf55bbbc475208d3e3cfb8
mark0725 <gaoxilin@gmail.com>
2024-10-30 16:59:26 +0800

    add app serve

--- app/serves.go
+++ app/serves.go
@@ -1,33 +1,82 @@
 package app
 
+import (
+	"context"
+	"fmt"
+)
+
 type IAppServe interface {
-	Start() error
-	Wait()
+	Done() <-chan struct{}
+	Start(context.Context) error
 	Ready() bool
 	Stop() error
 }
 
 type AppServe struct {
-	Name  string
-	Serve IAppServe
+	Name   string
+	Module string
+	Serve  IAppServe
 	//Done    chan Signal
 }
 
 var g_appServes = make(map[string]AppServe, 10)
 
-func RegisterServe(name string, serve IAppServe) {
-	g_appServes[name] = AppServe{
-		Name:  name,
-		Serve: serve,
+func GetServeNames() []string {
+	var names []string
+	for k := range g_appServes {
+		names = append(names, k)
+	}
+	return names
+}
+
+func GetServes() map[string]AppServe {
+	return g_appServes
+}
+
+func GetServe(name string) (AppServe, bool) {
+	v, ok := g_appServes[name]
+	return v, ok
+}
+
+func GetServeNamesByModule(module string) []string {
+	var names []string
+	for k, v := range g_appServes {
+		if v.Module == module {
+			names = append(names, k)
+		}
+	}
+	return names
+}
+
+func RegisterServe(name string, module string, serve IAppServe) {
+	id := fmt.Sprintf("%s.%s", module, name)
+	g_appServes[id] = AppServe{
+		Name:   name,
+		Module: module,
+		Serve:  serve,
 		//Done:    make(chan Signal),
 	}
 }
 
-func StartServes() error {
-	for _, v := range g_appServes {
-		err := v.Serve.Start()
+func StartServe(ctx context.Context, serveId string) error {
+	if v, ok := g_appServes[serveId]; ok {
+		err := v.Serve.Start(ctx)
 		if err != nil {
 			return err
+		}
+		return nil
+	}
+
+	return fmt.Errorf("serve %s not found", serveId)
+}
+
+func StartServes(ctx context.Context, serves []string) error {
+	for _, v := range serves {
+		if v, ok := g_appServes[v]; ok {
+			err := v.Serve.Start(ctx)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -35,6 +84,10 @@ func StartServes() error {
 
 func StopServes() error {
 	for _, v := range g_appServes {
+		if !v.Serve.Ready() {
+			continue
+		}
+
 		err := v.Serve.Stop()
 		if err != nil {
 			return err
