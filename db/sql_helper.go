0106759565736784706692ddac0038748afa41ea
mark0725 <gaoxilin@gmail.com>
2025-11-18 18:15:43 +0800

    fix: QueryParamsBuilder bug.

--- db/sql_helper.go
+++ db/sql_helper.go
@@ -3,6 +3,8 @@ package db
 import (
 	"fmt"
 	"strings"
+
+	"github.com/mark0725/go-app-base/utils"
 )
 
 type ISqlHelper interface {
@@ -68,6 +70,19 @@ func QueryNamedParamsBuilder(sqlOld string, params map[string]any) SqlBuildResul
 	return helper.QueryNamedParamsBuilder(sqlOld, params)
 }
 
+func QueryNamedParamsBuilder2(db string, sqlOld string, params map[string]any) SqlBuildResult {
+	conn := GetDBConn(db)
+	if conn == nil {
+		return SqlBuildResult{}
+	}
+	helper := g_DBHelpers[conn.Type]
+	if helper == nil {
+		return SqlBuildResult{}
+	}
+
+	return helper.QueryNamedParamsBuilder(sqlOld, params)
+}
+
 func PageQueryBuilder(db string, sql string, limit int, offset int) (string, error) {
 	conn := GetDBConn(db)
 	if conn == nil {
@@ -111,19 +126,40 @@ func GetSqlHelper(db string) ISqlHelper {
 	return g_DBHelpers[conn.Type]
 }
 
+type TYPE_WHERECLAUSE string
+
+const (
+	TYPE_WHERECLAUSE_STRING TYPE_WHERECLAUSE = "string"
+)
+
+type whereClause struct {
+	whereType TYPE_WHERECLAUSE
+	query     string
+	params    map[string]any
+	queryMap  map[string]any
+	fields    []string
+}
+
 type QueryParamsOptions struct {
 	sql           string
+	cols          []string
+	from          string
+	where         *whereClause
 	params        map[string]any
 	defaultPrefix string
 	otherPrefixes map[string][]string
 	spOps         map[string]string
 	groupBy       string
 	orderBy       string
+	limit         int
+	offset        int
 }
 
 func NewQueryParamsBuilder() *QueryParamsOptions {
 	return &QueryParamsOptions{
 		sql:           "",
+		cols:          []string{},
+		from:          "",
 		params:        make(map[string]any),
 		defaultPrefix: "",
 		otherPrefixes: make(map[string][]string),
@@ -140,10 +176,7 @@ func (q *QueryParamsOptions) OtherPrefixes(otherPrefixes map[string][]string) *Q
 	q.otherPrefixes = otherPrefixes
 	return q
 }
-func (q *QueryParamsOptions) Params(params map[string]any) *QueryParamsOptions {
-	q.params = params
-	return q
-}
+
 func (q *QueryParamsOptions) SpOps(spOps map[string]string) *QueryParamsOptions {
 	q.spOps = spOps
 	return q
@@ -154,40 +187,101 @@ func (q *QueryParamsOptions) Sql(sql string) *QueryParamsOptions {
 	return q
 }
 
-func (q *QueryParamsOptions) GroupBy(groupBy string) *QueryParamsOptions {
+func (q *QueryParamsOptions) Columns(cols ...string) *QueryParamsOptions {
+	q.cols = cols
+	return q
+}
+
+func (q *QueryParamsOptions) From(from string) *QueryParamsOptions {
+	q.from = from
+	return q
+}
+
+func (q *QueryParamsOptions) WhereString(query string, params map[string]any) *QueryParamsOptions {
+
+	q.where = &whereClause{
+		whereType: TYPE_WHERECLAUSE_STRING,
+		query:     query,
+		params:    params,
+	}
+	return q
+}
+
+func (q *QueryParamsOptions) WhereMap(params map[string]any, field ...string) *QueryParamsOptions {
+
+	for k, v := range params {
+		if len(field) > 0 && !utils.Contains(field, k) {
+			continue
+		}
+		q.params[k] = v
+	}
+
+	return q
+}
+
+func (q *QueryParamsOptions) Group(groupBy string) *QueryParamsOptions {
 	q.groupBy = groupBy
 	return q
 }
 
-func (q *QueryParamsOptions) OrderBy(orderBy string) *QueryParamsOptions {
+func (q *QueryParamsOptions) Order(orderBy string) *QueryParamsOptions {
 	q.orderBy = orderBy
 	return q
 }
 
-func (q *QueryParamsOptions) BuildSql() string {
-	sqlParts := q.Build()
-	sql := q.sql
-	if sqlParts != "" {
-		sql += " AND " + sqlParts
-	}
-	if q.groupBy != "" {
-		sql += " GROUP BY " + q.groupBy
-	}
-	if q.orderBy != "" {
-		sql += " ORDER BY " + q.orderBy
-	}
-	return sql
+func (q *QueryParamsOptions) Limit(limit int) *QueryParamsOptions {
+	q.limit = limit
+	return q
 }
 
-func (q *QueryParamsOptions) Build() string {
+func (q *QueryParamsOptions) Offset(offset int) *QueryParamsOptions {
+	q.offset = offset
+	return q
+}
+
+func (q *QueryParamsOptions) Build() (string, map[string]any) {
+	cols := "*"
+	if len(q.cols) > 0 {
+		cols = strings.Join(q.cols, ",")
+	}
+	sql := fmt.Sprintf("select %s from %s", cols, q.from)
+	sqlParams := map[string]any{}
+	if q.where != nil {
+		sql += " where " + q.where.query
+		for k, v := range q.where.params {
+			sqlParams[k] = v
+		}
+	} else {
+		sql += " where 1=1"
+	}
+
+	if q.sql != "" {
+		sql += q.sql
+	}
+
+	sqlParts, sqlParams := q.BuildSqlPart()
+	if sqlParts != "" {
+		sql += " and (" + sqlParts + ")"
+	}
+	if q.groupBy != "" {
+		sql += " group by " + q.groupBy
+	}
+	if q.orderBy != "" {
+		sql += " order by " + q.orderBy
+	}
+	return sql, sqlParams
+}
+
+func (q *QueryParamsOptions) BuildSqlPart() (string, map[string]any) {
 	sqlParts := []string{}
 	paramMap := make(map[string]any)
-	//paramCount := make(map[string]int)
+	paramCount := make(map[string]int)
 
-	getPlaceholder := func(key string) string {
-		//paramCount[key]++
-		// return fmt.Sprintf("{%s_%d}", key, paramCount[key])
-		return fmt.Sprintf("{%s}", key)
+	getPlaceholder := func(key string, v any) string {
+		paramCount[key]++
+		paramKey := fmt.Sprintf("%s_%d", key, paramCount[key])
+		paramMap[paramKey] = v
+		return fmt.Sprintf("{%s}", paramKey)
 	}
 
 	for k, val := range q.params {
@@ -206,34 +300,27 @@ func (q *QueryParamsOptions) Build() string {
 		}
 
 		spOp := ""
-		if q.spOps != nil {
+		if q.spOps != nil && q.spOps[k] != "" {
 			spOp = q.spOps[k]
-		}
-
-		buildLike := func(arr []any, single bool) string {
-			if single {
-				strVal, _ := arr[0].(string)
-				strVal = strings.ReplaceAll(strVal, "'", "")
-				return fmt.Sprintf("%s like '%%%s%%'", paraName, strVal)
-			}
-			partArr := []string{}
-			for i, v := range arr {
-				strVal, _ := v.(string)
-				strVal = strings.ReplaceAll(strVal, "'", "")
-				partArr = append(partArr, fmt.Sprintf("%s like '%%%s%%'", paraName, strVal))
-				arr[i] = strVal
-			}
-			return fmt.Sprintf("(%s)", strings.Join(partArr, " or "))
 		}
 
 		switch spOp {
 		case "like":
 			if arrVal, ok := val.([]any); ok && len(arrVal) > 0 {
-				sqlParts = append(sqlParts, buildLike(arrVal, false))
+				partArr := []string{}
+				for _, v := range arrVal {
+					strVal, _ := v.(string)
+					strVal = strings.ReplaceAll(strVal, "'", "")
+					ph := getPlaceholder(k, fmt.Sprintf("%%%s%%", strVal))
+					partArr = append(partArr, fmt.Sprintf("%s like %s", paraName, ph))
+				}
+
+				sqlParts = append(sqlParts, fmt.Sprintf("(%s)", strings.Join(partArr, " or ")))
 			} else {
 				strVal, _ := val.(string)
-				valArr := []any{strVal}
-				sqlParts = append(sqlParts, buildLike(valArr, true))
+				strVal = strings.ReplaceAll(strVal, "'", "")
+				ph := getPlaceholder(k, fmt.Sprintf("%%%s%%", strVal))
+				sqlParts = append(sqlParts, fmt.Sprintf("%s like %s", paraName, ph))
 			}
 		case "-":
 			// 区间
@@ -242,7 +329,7 @@ func (q *QueryParamsOptions) Build() string {
 					// paraName <= ?
 					strVal, _ := arrVal[0].(string)
 					strVal = strings.ReplaceAll(strVal, "-", "")
-					ph := getPlaceholder(k)
+					ph := getPlaceholder(k, strVal)
 					sqlParts = append(sqlParts, fmt.Sprintf("%s <= %s", paraName, ph))
 					paramMap[ph] = strVal
 				} else {
@@ -252,63 +339,70 @@ func (q *QueryParamsOptions) Build() string {
 					strVal1, _ := arrVal[1].(string)
 					strVal1 = strings.ReplaceAll(strVal1, "-", "")
 
-					ph0 := getPlaceholder(k)
-					ph1 := getPlaceholder(k)
+					ph0 := getPlaceholder(k, strVal0)
+					ph1 := getPlaceholder(k, strVal1)
 					sqlParts = append(sqlParts, fmt.Sprintf("(%s >= %s and %s <= %s)", paraName, ph0, paraName, ph1))
-					paramMap[ph0] = strVal0
-					paramMap[ph1] = strVal1
 				}
 			} else {
 				strVal, _ := val.(string)
 				if !strings.Contains(strVal, "-") {
-					ph := getPlaceholder(k)
+					ph := getPlaceholder(k, strings.ReplaceAll(strVal, "-", ""))
 					sqlParts = append(sqlParts, fmt.Sprintf("%s <= %s", paraName, ph))
-					paramMap[ph] = strings.ReplaceAll(strVal, "-", "")
 				}
+			}
+		case "in":
+			// in
+			if arrVal, ok := val.([]any); ok {
+				placeholders := []string{}
+				for _, one := range arrVal {
+					ph := getPlaceholder(k, one)
+					placeholders = append(placeholders, ph)
+				}
+				sqlParts = append(sqlParts, fmt.Sprintf("%s in(%s)", paraName, strings.Join(placeholders, ",")))
 			}
 		case "not in":
 			// not in
 			if arrVal, ok := val.([]any); ok {
 				placeholders := []string{}
 				for _, one := range arrVal {
-					ph := getPlaceholder(k)
+					ph := getPlaceholder(k, one)
 					placeholders = append(placeholders, ph)
-					paramMap[ph] = one
 				}
 				sqlParts = append(sqlParts, fmt.Sprintf("%s not in(%s)", paraName, strings.Join(placeholders, ",")))
 			}
+		case "is null":
+			sqlParts = append(sqlParts, fmt.Sprintf("%s %s ?", paraName, spOp))
+		case "is not null":
+			sqlParts = append(sqlParts, fmt.Sprintf("%s %s ?", paraName, spOp))
 		default:
 			// no special op or other operators
-			if spOp != "" && spOp != "-" && spOp != "not in" && spOp != "like" {
+			if spOp != "" {
 				// paraName + spOps[k] + ?
-				ph := getPlaceholder(k)
+				ph := getPlaceholder(k, val)
 				sqlParts = append(sqlParts, fmt.Sprintf("%s %s %s", paraName, spOp, ph))
-				paramMap[ph] = val
 			} else {
 				// in or default =
 				if arrVal, ok := val.([]any); ok {
 					placeholders := []string{}
 					for _, one := range arrVal {
-						ph := getPlaceholder(k)
+						ph := getPlaceholder(k, one)
 						placeholders = append(placeholders, ph)
-						paramMap[ph] = one
 					}
 					sqlParts = append(sqlParts, fmt.Sprintf("%s in(%s)", paraName, strings.Join(placeholders, ",")))
 				} else {
-					ph := getPlaceholder(k)
+					ph := getPlaceholder(k, val)
 					sqlParts = append(sqlParts, fmt.Sprintf("%s = %s", paraName, ph))
-					paramMap[ph] = val
 				}
 			}
 		}
 	}
 
-	return strings.Join(sqlParts, " and ")
+	return strings.Join(sqlParts, " and "), paramMap
 }
 
-func QueryParamsBuilder(params map[string]any, defaultPrefix string, otherPrefixes map[string][]string, spOps map[string]string) string {
-	return NewQueryParamsBuilder().Params(params).DefaultPrefix(defaultPrefix).OtherPrefixes(otherPrefixes).SpOps(spOps).Build()
-}
+// func QueryParamsBuilder(params map[string]any, defaultPrefix string, otherPrefixes map[string][]string, spOps map[string]string) string {
+// 	return NewQueryParamsBuilder().Params(params).DefaultPrefix(defaultPrefix).OtherPrefixes(otherPrefixes).SpOps(spOps).Build()
+// }
 
 func RegisterSqlHelper(dbType string, helper ISqlHelper) {
 	g_DBHelpers[dbType] = helper
